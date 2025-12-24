050d47937feff4aa83edb646f1ee61af1d7fb232
Ming Huang <mingh1002@gmail.com>
2025-12-23 21:36:29 -0500

    Set basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // basePath: "/momo-site",
+  basePath: "/momo-site",
   output: "export",
   images: {
     remotePatterns: [new URL('https://tecdn.b-cdn.net/**')],
